fa437104e6941e88b49648ddedcd89a2990446ff
Aditi Inamdar <66082809+aditiinamdar@users.noreply.github.com>
2025-12-03 22:19:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 function insert_Row() {
     //Write your code here
+	let tableid=document.getElementById("sampleTable");
+	tableid.insertAdjacentHTML("afterbegin","<tr><td>New cell1</td> <td>New cell2</td></tr> ");
   
   
 }
